5938ccb10b07cbba4f41d20da35181e32e9b8ecb
mad <bimnaxu753@gmail.com>
2022-08-25 16:27:14 +0700

    fix bash

--- convert_h5_to_pd/run.sh
+++ convert_h5_to_pd/run.sh
@@ -1,2 +1,2 @@
 docker run --rm -it --name mar -v $(pwd)/model_output:/model_output -v \
-$(pwd)/model_input:/model_input tfserving:build_retina python convert.pys
+$(pwd)/model_input:/model_input tfserving:build_retina python convert.py
